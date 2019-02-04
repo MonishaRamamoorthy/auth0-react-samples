ed02445637117b63e181292ee294f2588652e7ad
MonishaRamamoorthy <46843348+MonishaRamamoorthy@users.noreply.github.com>
2019-02-04 09:03:59 +0530

    Create vowels.py

---
+++ 01-Login/vowels.py
@@ -0,0 +1,7 @@
+ch=input()
+if(ch=='a'or ch=='A'or ch=='e'or ch=='E'or ch=='i'or ch=='I'or ch=='o'or ch=='u'or ch=='U'):
+	print("Vowel")
+elif(ch>='a'and ch>='z')or(ch>='A'and ch>='Z'):
+	print("Consonant")
+else:
+	print("invalid")
